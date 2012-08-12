9905cc0b5a70f9c4bcee48664788f4181efc1fe9
Maxim Colls <collsmaxim@gmail.com>
2012-08-12 12:01:16 +0200

    Add some comments

--- client/users.js
+++ client/users.js
@@ -5,11 +5,14 @@
 
 Meteor.startup(function() {
 
+  // Subscribe to users Collection
   Meteor.subscribe("users");
 
+  // Create new user (since I'm not using any cookie I cannot identify retourning visitors)
   user_id = Users.insert({name: '', last_keepalive: (new Date()).getTime()});
   Session.set('user_id', user_id);
 
+  // Keep the user alive
   Meteor.setInterval(function() {
     if (Meteor.status().connected) {
       Meteor.call('keepalive', Session.get('user_id'));
--- server/users.js
+++ server/users.js
@@ -18,7 +18,9 @@ Meteor.methods({
   keepalive: function (user_id) {
     var now = (new Date()).getTime();
     var remove_threshold = now - 8*1000;
+    // Updates current user last_keepalive attribute
     Users.update({_id: user_id }, { $set: { last_keepalive: now } });
+    // Remove old users (this should be triggered in the server but I had some problems with it)
     Users.remove({ 'last_keepalive': { $lt: remove_threshold }});
   }
 });
